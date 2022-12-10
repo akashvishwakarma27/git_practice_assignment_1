45d84bc63271157aed8f9ce64c1755c0597851fd
akashvishwakarma27 <akashvish4masai@gmail.com>
2022-12-10 16:58:12 +0530

    problem

---
+++ prime.js
@@ -0,0 +1,15 @@
+num = 13;
+
+let count = 0;
+
+for (let i=1; i<=num; i++){
+    if (num%i===1){
+        count++;
+    }
+}
+
+if (count == 2){
+    console.log("Yes")
+}else {
+    console.log("No");
+}
